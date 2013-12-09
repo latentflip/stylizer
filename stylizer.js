7c5e20d7190f4d42392b280e95cbcd0ad8db1610
Philip Roberts <phil@andyet.net>
2013-12-09 18:16:04 +0000

    Require stylus + plugin support

--- stylizer.js
+++ stylizer.js
@@ -1,13 +1,37 @@
 var stylus = require('stylus');
 var fs = require('fs');
 var path = require('path');
+var prequire = require('parent-require');
+var util = require('util');
 
-module.exports = function (infile, outfile, done) {
+module.exports = function (infile, outfile, plugins, done) {
+
+    if (!done) {
+        if (typeof plugins === 'function') {
+            done = plugins;
+            plugins = [];
+        }
+    }
+
     var styl = fs.readFileSync(infile);
-    stylus(styl.toString())
-    .set('paths', [ path.dirname(infile) ])
-    .set('include css', true)
-    .render(function (err, css) {
+
+    var compiler = stylus(styl.toString())
+                    .set('paths', [ path.dirname(infile) ])
+                    .set('include css', true);
+
+    if (util.isArray(plugins)) {
+        plugins.forEach(function (plugin) {
+            var p = prequire(plugin);
+            compiler.use(p());
+        });
+    } else {
+        Object.keys(plugins).forEach(function (plugin) {
+            var p = prequire(plugin);
+            compiler.use(p(plugins[plugin]));
+        });
+    }
+
+    compiler.render(function (err, css) {
         if (err) throw err;
 
         fs.writeFile(outfile, css, done);
